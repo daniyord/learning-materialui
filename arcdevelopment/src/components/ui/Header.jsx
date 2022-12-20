48b88f67f2430957768a1c553dcce72ea1eb659b
Daniel Yordanov <daniel.yordanov@a1.bg>
2022-12-20 14:00:56 +0200

    -

--- arcdevelopment/src/components/ui/Header.jsx
+++ arcdevelopment/src/components/ui/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { AppBar, Toolbar, useScrollTrigger } from "@material-ui/core";
 import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 const ElevationScroll = (props) => {
   const { children } = props;
@@ -15,15 +16,26 @@ const ElevationScroll = (props) => {
   });
 };
 
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+  },
+}));
+
 const Header = () => {
+  const classes = useStyles();
+
   return (
-    <ElevationScroll>
-      <AppBar position="fixed">
-        <Typography variant="h3">
-          <Toolbar>Arc Development</Toolbar>
-        </Typography>
-      </AppBar>
-    </ElevationScroll>
+    <>
+      <ElevationScroll>
+        <AppBar position="fixed">
+          <Typography variant="h3">
+            <Toolbar>Arc Development</Toolbar>
+          </Typography>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </>
   );
 };
 
